9d131e05e9b163ecc5792c5267c7d77cced96614
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-03-02 11:22:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from pyrogram import Client, filters
 import requests
 import os
 import re
+from urllib.parse import unquote
 
 # Set your API credentials
 api_id = "10471716"
 api_hash = "f8a1b21a13af154596e2ff5bed164860"
 bot_token = "6365859811:AAGK5hlLKtLf-RqlaEXngZTWnfSPISerWPI"
-# Create the Pyrogram client
+
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 # Define a start message handler
@@ -24,7 +25,7 @@ def download_file(client, message):
         link = message.text.split(None, 1)[1]
 
         # Determine the filename using Content-Disposition
-        response = requests.head(link)
+        response = requests.head(link, stream=True)
         content_disposition = response.headers.get('Content-Disposition')
         if content_disposition and 'filename' in content_disposition:
             original_filename = content_disposition.split('filename=')[1].strip('\"')
@@ -37,9 +38,12 @@ def download_file(client, message):
             return
 
         # Download the file using requests
-        with open(filename, 'wb') as file:
-            response = requests.get(link)
-            file.write(response.content)
+        with requests.get(link, stream=True) as response:
+            response.raise_for_status()
+            with open(filename, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:
+                        file.write(chunk)
 
         # Send the downloaded file to the user
         message.reply_document(document=filename)
@@ -50,6 +54,10 @@ def download_file(client, message):
     except IndexError:
         # Handle the case where the command is missing the link
         message.reply_text("Please provide a valid link after the /download command.")
+
+    except requests.exceptions.RequestException as e:
+        # Handle connection issues or other requests-related errors
+        message.reply_text(f"An error occurred: {str(e)}")
 
     except Exception as e:
         # Handle other exceptions
